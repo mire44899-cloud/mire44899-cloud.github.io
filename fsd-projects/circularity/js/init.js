efa877e0447eb59bad6ffdf42bebcfc3c607cf53
mire44899-cloud <mire44899@students.apsb.org>
2026-02-23 16:37:29 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,19 +20,32 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
+            var circle;
+            var circles = [];
 
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas, 5, 5);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
 
         // TODO 3 : Call the drawCircle() function
-
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
 
         // TODO 7 : Use a loop to create multiple circles
-
+         for(var i = 0; i < 400; i++){
+            drawCircle();
+         }
 
 
 
@@ -47,13 +60,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-
-            
+               
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
+              
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
+           for(var i = 0; i < circles.length; i++){
+             physikz.updatePosition(circles[i]);
+             game.checkCirclePosition(circles[i]);
+           }
             
         }
     
@@ -70,9 +85,18 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
-
+            //circle passing LEFT side of the screen
+            if(circle.x < 0){
+              circle.x = canvas.width;
+            }
+            //circle passing TOP of the screen
+            if(circle.y < 0){
+                circle.y = canvas.height;
+            }
+            //circle passing the BOTTOM of the screen
+            if(circle.y > canvas.height){
+              circle.y = 0;
+            }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
